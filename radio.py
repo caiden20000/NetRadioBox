f9fb0dfd23801de75512e7718868d0b3d64a196d
c w <caiden2000offline@gmail.com>
2024-08-29 06:46:16 +0000

    Implement everything in Clock except alarm activation

--- radio.py
+++ radio.py
@@ -48,6 +48,7 @@ import OLED_1in51 # Located in libdir
 from PIL import Image,ImageDraw,ImageFont
 from enum import Enum
 from types import FunctionType as function
+from datetime import datetime, timedelta
 
 # TODO: Mode resets to station after amount of time
 # TODO: Auto timezone support based on wifi location
@@ -364,66 +365,75 @@ class Encoder:
 
 
 
-# Time stored as SECONDS
 # Since we always have ms since epoch, "setting the time" should be as an offset to that.
 # Alarm is stored as minutes from 0000 (midnight) 
 # TODO: Implement all methods
-MS_IN_DAY = 1000 * 60 * 60 * 24
-SECONDS_IN_DAY = 60 * 60 * 24
+MINUTES_IN_DAY = 60 * 24
+SECONDS_IN_DAY = 60 * MINUTES_IN_DAY
 class Clock:
     def __init__(self):
-        # Offset from UTC (in seconds)
+        # Offset from system time (in seconds)
         self.current_time_offset = 0
-        # Time in seconds of alarm time. 0 < alarm_time < SECONDS_IN_DAY
+        # Alarm time in MINUTES from midnight.
         self.alarm_time = 0
         self.alarm_active = False
         self.alarm_callback = None
         self.alarm_thread = None
 
-    def _active_alarm(self):
-        # if self.alarm_active is False:
-        #     return
-        # self.alarm_callback()
-        # self._init_alarm()
-        pass
+    def _seconds_through_day(self) -> int:
+        now = datetime.now()
+        start_of_day = datetime(year=now.year, month=now.month, day=now.day)
+        time_difference = now - start_of_day
+        return time_difference.total_seconds()
+
+    def _get_time_from_seconds_through_day(self, total_seconds: int) -> tuple[int, int, int]:
+        total_seconds %= SECONDS_IN_DAY
+        total_minutes = total_seconds // 60
+        total_hours = total_minutes // 60
+        seconds = total_seconds % 60
+        minutes = total_minutes % 60
+        hours = total_hours % 24
+        return (hours, minutes, seconds)
     
-    def _init_alarm(self):
-        # if self.alarm_thread is not None:
-        #     self.alarm_thread.cancel()
-        # seconds_until_alarm = 0 # TODO
-        # self.alarm_thread = threading.Timer(seconds_until_alarm, self._active_alarm)
-        # self.alarm_thread.start()
-        pass
+    def _get_time_from_minutes_through_day(self, total_minutes: int) -> tuple[int, int, int]:
+        return self._get_time_from_seconds_through_day(total_minutes * 60)
 
     def set_time_to_system_time(self) -> None:
         self.current_time_offset = 0
+
     def set_current_time_offset(self, new_time_seconds: int) -> None:
-        pass # TODO
+        self.current_time_offset = new_time_seconds % SECONDS_IN_DAY
         
     def scrub_current_time_offset(self, change_seconds: int) -> None:
         self.set_current_time_offset(self.current_time_offset + change_seconds)
 
-    def set_alarm_time(self, new_time_seconds: int) -> None:
-        # self.alarm_time = new_time_seconds % SECONDS_IN_DAY
-        pass
-    def scrub_alarm_time(self, change_seconds: int) -> None:
-        self.set_alarm_time((self.alarm_time + change_seconds) % SECONDS_IN_DAY)
+    def set_alarm_time(self, new_time_minutes: int) -> None:
+        self.alarm_time = new_time_minutes % MINUTES_IN_DAY
+
+    def scrub_alarm_time(self, change_minutes: int) -> None:
+        self.set_alarm_time(self.alarm_time + change_minutes)
+
     def set_alarm_active(self, is_alarm_active: bool) -> None:
-        # self.alarm_active = is_alarm_active
-        # if self.alarm_active is False and self.alarm_thread is not None:
-        #     self.alarm_thread.cancel()
-        #     self.alarm_thread = None
-        # if self.alarm_active is True:
-        #     self._init_alarm()
-        pass
+        pass # TODO
     def set_alarm_callback(self, callback: function) -> None:
         pass # TODO
+
+    def _get_offset_time(self) -> int:
+        return time.gmtime() + self.current_time_offset
     
-    # Gives time in HH:MM
     def get_current_time_string(self, with_colon: bool = True):
-        return time.strftime(f'%H{":" if with_colon else " "}%M', time.localtime())
+        if with_colon:
+            prestring = time.strftime('%H:%M', _get_offset_time())
+        else:
+            prestring = time.strftime('%H %M', _get_offset_time())
+        # Rip one leading zero (eg 01:00 -> 1:00, but 00:00 -> 0:00)
+        if prestring[0] == '0':
+            prestring[0] = ' '  # Replace with space otherwise break monospace layout
+        return prestring
+    
     def get_alarm_time_string(self):
-        return "00:00"
+        time_components = self._get_time_from_minutes_through_day(self.alarm_time)
+        return f'{time_components[0]:2d}:{time_components[1]:02d}'
     def get_alarm_active(self):
         return self.alarm_active
 
